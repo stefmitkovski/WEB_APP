cd11b1b5496edba6f56af9411665f52b466d9f3c
mihaelalazetic <mihaela.lazetic@gmail.com>
2022-10-04 08:53:31 +0200

    cart.php done

--- public/products/cart.php
+++ public/products/cart.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once '../../views/partials/navbar.php';
 ?>
 <?php if (isset($_COOKIE["cart"])) : ?>
@@ -14,25 +14,59 @@ require_once '../../views/partials/navbar.php';
 
   <section class="h-100 gradient-custom">
     <div class="container py-5">
+      <div class="col-md-4 float-right" style="top: 25px; margin-bottom: 20px;">
+        <div class="card mb-4">
+          <div class="card-header py-3">
+            <h5 class="mb-0">Summary</h5>
+          </div>
+          <div class="card-body">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+                Products
+                <span>$<?php echo $total; ?></span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center px-0">
+                Shipping
+                <span>Free</span>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                <div>
+                  <strong>Total amount</strong>
+                  <strong>
+                    <p class="mb-0">(including 20% VAT)</p>
+                  </strong>
+                </div>
+                <span><strong>$<?php echo $total * 1.2 ?></strong></span>
+              </li>
+            </ul>
+
+            <a href="checkout.php" type="button" class="btn btn-primary btn-lg btn-block">
+              Go to checkout
+            </a>
+          </div>
+        </div>
+      </div>
+
       <div class="row d-flex justify-content-center my-4">
-        <div class="col-md-8">
+
+        <div class="col-md-12">
           <div class="card mb-4">
             <div class="card-header py-3">
               <h5 class="mb-0">Cart - <?php
-                            if (isset($_COOKIE["cart"])) {
-                                $cart = json_decode($_COOKIE["cart"]);
-                                $total = 0;
-                                foreach ($cart as $c) {
-                                    $total = $total + $c->quantity;
-                                }
-                                echo $total;
-                            } else {
-                                echo "0";
-                            }
-                            ?> items</h5>
+                                      if (isset($_COOKIE["cart"])) {
+                                        $cart = json_decode($_COOKIE["cart"]);
+                                        $total = 0;
+                                        foreach ($cart as $c) {
+                                          $total = $total + $c->quantity;
+                                        }
+                                        echo $total;
+                                      } else {
+                                        echo "0";
+                                      }
+                                      ?> items</h5>
             </div>
             <?php foreach ($cart as $c) : ?>
-              <?php $prod = $product->getSpecific($c->productID);?>
+              <?php $prod = $product->getSpecific($c->productID); ?>
               <div class="card-body">
                 <!-- Single item -->
                 <div class="row">
@@ -66,7 +100,7 @@ require_once '../../views/partials/navbar.php';
                       <div class="d-flex mb-5  p-2" style="max-width: 300px">
                         <input type="hidden" name="decrement" value="<?php echo $c->productID ?>">
                         <button class="btn btn-primary px-3 me-2 h-100" onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                            <i class="fa fa-minus"></i>
+                          <i class="fa fa-minus"></i>
                         </button>
                     </form>
                     <div class="form-outline">
@@ -96,26 +130,26 @@ require_once '../../views/partials/navbar.php';
                   <!-- Price -->
                 </div>
               </div>
+              <hr class="my-4" />
               <!-- Single item -->
             <?php endforeach; ?>
-            <hr class="my-4" />
           </div>
         </div>
-        <div class="card mb-4">
+      </div>
+      <div class="card mb-4">
           <div class="card-body">
             <p><strong>Expected shipping delivery</strong></p>
-            
+
             <p class="mb-0" id="date"></p>
             <script>
-                 let currentdate = new Date();
-                let onemonth = new Date(currentdate.setMonth(currentdate.getMonth()+1));
-                let threemonth = new Date(currentdate.setMonth(currentdate.getMonth()+3));
-                const D = new Date(onemonth);
-                  const C = new Date(threemonth);
-                let ff=(D.getDate())+"."+(D.getMonth() + 1)+"."+(D.getFullYear());  
-                let bb=(C.getDate())+"."+(C.getMonth() + 1)+"."+(C.getFullYear()); 
-              $("#date").append(ff + " - " +bb );
-
+              let currentdate = new Date();
+              let onemonth = new Date(currentdate.setMonth(currentdate.getMonth() + 1));
+              let threemonth = new Date(currentdate.setMonth(currentdate.getMonth() + 3));
+              const D = new Date(onemonth);
+              const C = new Date(threemonth);
+              let ff = (D.getDate()) + "." + (D.getMonth() + 1) + "." + (D.getFullYear());
+              let bb = (C.getDate()) + "." + (C.getMonth() + 1) + "." + (C.getFullYear());
+              $("#date").append(ff + " - " + bb);
             </script>
           </div>
         </div>
@@ -128,39 +162,6 @@ require_once '../../views/partials/navbar.php';
             <img class="me-2" width="50px" src="\public\images\paypal.png" alt="PayPal acceptance mark" />
           </div>
         </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card mb-4">
-          <div class="card-header py-3">
-            <h5 class="mb-0">Summary</h5>
-          </div>
-          <div class="card-body">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                Products
-                <span>$<?php echo $total; ?></span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center px-0">
-                Shipping
-                <span>Free</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
-                <div>
-                  <strong>Total amount</strong>
-                  <strong>
-                    <p class="mb-0">(including  20% VAT)</p>
-                  </strong>
-                </div>
-                <span><strong>$<?php echo $total * 1.2 ?></strong></span>
-              </li>
-            </ul>
-
-            <a href="checkout.php" type="button" class="btn btn-primary btn-lg btn-block">
-              Go to checkout
-            </a>
-          </div>
-        </div>
-      </div>
     </div>
     </div>
   </section>
@@ -175,8 +176,6 @@ require_once '../../views/partials/navbar.php';
       <a href="index.php" class="btn btn-primary">Explore Products</a>
     </div>
   </div>
-
-
 <?php endif; ?>
 
 <footer class="py-5 bg-dark">
